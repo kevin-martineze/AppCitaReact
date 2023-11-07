a2daaa5b47ac989ebd0b44c83262cb76eb571f2f
Kevin Martínez <54540601+kevin-martineze@users.noreply.github.com>
2023-11-06 22:21:44 -0500

    Cambiando titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 function Header() {    
     return (
         <>
-            <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">Seguimiento <span className="text-indigo-600">Pacientes con Github</span></h1>
+            <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">Seguimiento <span className="text-indigo-600">Pacientes con Githubb</span></h1>
         </>        
     )
 }
